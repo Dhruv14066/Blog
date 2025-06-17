23b6a9279e6f9cb49e4b47042401e1fe47c827ee
Dhruv14066 <dhruvdhanuka315@gmail.com>
2025-06-17 17:50:52 +0530

    made custom wrappers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error",(error)=>{
+        console.log("Error : ", error);
+        throw error;
+    })
+    app.listen(process.env.port || 8000, () => {
+            console.log(`application is runing at port :  ${process.env.PORT}`)
+        })
+    })
+.catch((error) => {
+    console.log("MongoDB connection failed !!!!", error);
+})
